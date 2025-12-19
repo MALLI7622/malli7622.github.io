ccbbddedf5a157bb2ce75e8b962aac2eba49bc4b
MALLI7622 <tmallikarjuna111@gmail.com>
2025-12-19 12:52:23 +0000

    Deploying to gh-pages from @ MALLI7622/malli7622.github.io@3ed2ea9b7336fe5b40987ec67977c72e2547dd21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,11 +462,39 @@ ninja.data = [{
           window.open("mailto:%74%6D%61%6C%6C%69%6B%61%72%6A%75%6E%61%31%31%31%31@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/MALLI7622", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/cherie_arjun", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/neuralnerd", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=36baViwAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/_neuralnerd_", "_blank");
         },
       },{
       id: 'light-theme',
